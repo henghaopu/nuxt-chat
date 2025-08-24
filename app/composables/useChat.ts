ff0c304e887893e5762a3ede8430c3e3509e26a9
Heng-Hao Pu <henghaopu@gmail.com>
2025-08-24 19:57:02 -0400

    feat(composables): add mock data and chat composable

---
+++ app/composables/useChat.ts
@@ -0,0 +1,27 @@
+import type { Chat, ChatMessage, Role } from '../types'
+import { MOCK_CHAT } from './mockData'
+
+export default function useChat() {
+  const chat = ref<Chat>(MOCK_CHAT)
+  const messages = computed<ChatMessage[]>(() => chat.value.messages)
+
+  function createMessage(message: string, role: Role) {
+    const id = messages.value.length.toString()
+
+    return { id, role, content: message }
+  }
+
+  function sendMessage(message: string) {
+    messages.value.push(createMessage(message, 'user'))
+
+    setTimeout(() => {
+      messages.value.push(createMessage(`You said: ${message}`, 'assistant'))
+    }, 200)
+  }
+
+  return {
+    chat,
+    messages,
+    sendMessage,
+  }
+}
