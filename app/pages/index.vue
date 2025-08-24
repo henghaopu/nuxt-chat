f4c0ccf7e0489429492763e5fdcce9d1799cdfe0
Heng-Hao Pu <henghaopu@gmail.com>
2025-08-23 23:41:04 -0400

    feat: enable reusable layouts by wrapping NuxtPage in NuxtLayout and add default layout component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <UContainer class="flex flex-col items-center justify-center h-svh">
+  <UContainer class="flex flex-col items-center justify-center h-screen">
     <h1 class="text-4xl font-bold">Welcome to Nuxt Chat</h1>
     <UButton to="/chat" class="mt-4">Start Chat</UButton>
   </UContainer>
