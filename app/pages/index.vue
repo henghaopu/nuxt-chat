9f626b916bdeef2183ffcf3c86af319d013373a3
Heng-Hao Pu <henghaopu@gmail.com>
2025-08-23 21:16:06 -0400

    feat: create pages with file-based routing and the NuxtPage component

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <UContainer class="flex flex-col items-center justify-center h-svh">
+    <h1 class="text-4xl font-bold">Welcome to Nuxt Chat</h1>
+    <UButton to="/chat" class="mt-4">Start Chat</UButton>
+  </UContainer>
+</template>
