268c685ee2781930c24c3d57eff657b64c317f08
Heng-Hao Pu <henghaopu@gmail.com>
2025-09-01 23:58:48 -0400

    chore: add @nuxtjs/mdc as a dev dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxtjs/mdc'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     // api secrets
